5d55d78bb94ad37e22c984546ec3b81aebc82825
Jayden Chen <chenzhenhao@thinktrader.net>
2017-04-19 21:06:07 +0800

    update

--- HoursingInfoCrawler/spiders/douban_spider.py
+++ HoursingInfoCrawler/spiders/douban_spider.py
@@ -1,14 +1,19 @@
 # -*- coding: utf-8 -*-
 
-import Scrapy
+import scrapy
 
-class DoubanSpider(Scrapy.Spider):
+
+class DoubanSpider(scrapy.Spider):
     name = "douban"
     start_urls = [
         "https://www.douban.com/group/106955/discussion?start=0",
         "https://www.douban.com/group/106955/discussion?start=25",
         "https://www.douban.com/group/106955/discussion?start=50"
     ]
+
+    def start_requests(self):
+        for url in self.start_urls:
+            yield scrapy.Request(url=url, callback=self.parse, cookies=)
 
     def parse(self, response):
         pass
