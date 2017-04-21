0660981095407d9a11b9b90ff521e53d25c49639
Jayden Chen <chenzhenhao@thinktrader.net>
2017-04-21 15:59:47 +0800

    simple crawler

--- HoursingInfoCrawler/spiders/douban_spider.py
+++ HoursingInfoCrawler/spiders/douban_spider.py
@@ -1,19 +1,20 @@
 # -*- coding: utf-8 -*-
 
 import scrapy
-
+from HoursingInfoCrawler import config
 
 class DoubanSpider(scrapy.Spider):
     name = "douban"
-    start_urls = [
-        "https://www.douban.com/group/106955/discussion?start=0",
-        "https://www.douban.com/group/106955/discussion?start=25",
-        "https://www.douban.com/group/106955/discussion?start=50"
-    ]
+    total_page = 20
+    start_urls = ["https://www.douban.com/group/futianzufang/discussion?start={}".format(i*25) for i in range(total_page)]
 
     def start_requests(self):
         for url in self.start_urls:
-            yield scrapy.Request(url=url, callback=self.parse, cookies=)
+            yield scrapy.Request(url=url, callback=self.parse, cookies=config.DOUBAN_COOKIES, headers=config.DOUBAN_HEADERS)
 
     def parse(self, response):
-        pass
+        for item in response.xpath('//table[@class="olt"]/tr/td[@class="title"]/a'):
+            title = item.xpath('./@title').extract_first()
+            if "石厦" in title:
+                link = item.xpath('./@href').extract_first()
+                print("Title: {} ---- Link: {}".format(title, link))
